6b72b48ef600a2887187bb580341c500bfbc3916
Rafał Maciasz <rafalmaciasz@student.agh.edu.pl>
2021-11-06 14:09:44 +0100

    Uzupełnienie klas w main (niedokończone)

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from typing import Optional
+from typing import Optional, List, TypeVar, Dict
+from abc import ABC, abstractmethod
 
 
 class Product:
     # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
 
-    def __init__(self, name, price):
+    def __init__(self, name: str, price: float):
         self.name: str = name
         self.price: float = price
 
     def __eq__(self, other):
-        return None  # FIXME: zwróć odpowiednią wartość
+        if isinstance(other, Product):
+            return self.name == other.name and self.price == other.price  # FIXME: zwróć odpowiednią wartość
+        return False
 
     def __hash__(self):
         return hash((self.name, self.price))
 
 
+class Error(Exception):
+    pass
 
-
-class TooManyProductsFoundError:
+class TooManyProductsFoundError(Error):
     # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
     pass
 
@@ -30,12 +34,38 @@ class TooManyProductsFoundError:
 #   (2) możliwość odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę wyników wyszukiwania,
 #   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
 
-class ListServer:
-    pass
+class Server(ABC):
+    n_max_returned_entries = 5
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+    def get_entries(self, n_letters: int = 1) -> List[Product]:
+        name_pat = '^[a-zA-Z]{{{0}}}\\d{{3,3}}$'.format(n_letters)
+        entries = [product for product in self.all_products(n_letters) if ]
+
+    @abstractmethod
+    def all_products(self, n_letters: int = 1) -> List[Product]:
+        raise NotImplementedError
 
 
-class MapServer:
-    pass
+BaseOrDerivedT = TypeVar('BaseOrDerivedT', bound=Server)
+
+class ListServer(Server):
+    def __init__(self, products: List[Product], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.products: List[Product] = products
+
+    def all_products(self, n_letters: int = 1) -> List[Product]:
+        return self.products
+
+
+class MapServer(Server):
+    def __init__(self, products: List[Product], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.products: Dict[str, Product] = {product.name: product.price for product in products}
+
+    def all_products(self, n_letters: int = 1) -> List[Product]:
+        return list(self.products.values())
 
 
 class Client:
